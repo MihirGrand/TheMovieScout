7f2558ff6deca61a15951f4997669c1b00d9cda3
Mihir <118926177+MihirGrand@users.noreply.github.com>
2024-06-16 18:43:50 +0530

    updated movie and person details

--- app/movieDetail.jsx
+++ app/movieDetail.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useGlobalContext } from "../context/GlobalProvider";
 import {
   View,
   Text,
@@ -19,13 +20,15 @@ import { StatusBar } from "expo-status-bar";
 
 const MovieDetail = () => {
   const { deets, creds, recs } = useLocalSearchParams();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [tomatometer, setTomatometer] = useState("");
   const [imdb, setImdb] = useState("");
+  const [providers, setProviders] = useState([]);
   const navigation = useNavigation();
   const details = JSON.parse(deets);
   const credits = JSON.parse(creds);
   const recoms = JSON.parse(recs).results;
+  const recommendations = recoms.sort((a, b) => b.popularity - a.popularity);
   let desc = details.overview.split(".");
   let overview = desc.slice(0, 2).join(". ").trim();
   if (overview.length < 300 && details.overview.length > 300) {
@@ -35,6 +38,8 @@ const MovieDetail = () => {
   if (!overview.endsWith("?") && !overview.endsWith(".")) {
     overview += ".";
   }
+
+  const { region } = useGlobalContext();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -46,10 +51,15 @@ const MovieDetail = () => {
           fetch(`${process.env.EXPO_PUBLIC_API_URL}/im?id=${details.imdb_id}`, {
             method: "POST",
           }),
+          fetch(`https://api.themoviedb.org/3/movie/${details.id}/watch/providers`, options),
         ]);
-        const [rottenResponse, imResponse] = responses;
-        const rotten = (await rottenResponse.json()).score;
-        const imdb = (await imResponse.json()).rating;
+        const [rottenResponse, imResponse, watchResponse] = responses;
+        const watch = await watchResponse.json();
+        if (watch.results.IN) {
+          setProviders(watch.results.IN.flatrate);
+        }
+        const rotten = rottenResponse.status != 500 ? (await rottenResponse.json()).score : null;
+        const imdb = imResponse.status != 500 ? (await imResponse.json()).rating : null;
         setTomatometer(rotten);
         setImdb(imdb);
         setLoading(false);
@@ -75,12 +85,18 @@ const MovieDetail = () => {
     setLoading(true);
     try {
       const personUrl = `https://api.themoviedb.org/3/person/${item.id}?language=en-US`;
-      const response = await fetch(personUrl, options);
-      const Json = await response.json();
+      const creditsUrl = `https://api.themoviedb.org/3/person/${item.id}/movie_credits?language=en-US`;
+      const responses = await Promise.all([fetch(personUrl, options), fetch(creditsUrl, options)]);
+      const [detailsResponse, creditsResponse] = responses;
+      const detailsJson = await detailsResponse.json();
+      const credJson = await creditsResponse.json();
       setLoading(false);
       router.push({
         pathname: "personDetail",
-        params: { deets: JSON.stringify(Json) },
+        params: {
+          deets: JSON.stringify(detailsJson),
+          creds: JSON.stringify(credJson),
+        },
       });
     } catch (error) {
       console.error("Error fetching person details:", error);
@@ -121,27 +137,39 @@ const MovieDetail = () => {
 
   const renderRec = ({ item }) => (
     <TouchableHighlight underlayColor={"transparent"} onPress={() => handleMoviePress(item)}>
-      <View key={item.id} className="w-24 p-2">
+      <View key={item.id} className="w-28 p-2">
         <Image
           className="h-40 rounded-md"
           source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
           resizeMode="cover"
         />
-        <Text className="font-qbold text-wrap text-center">{item.title}</Text>
+        <Text
+          className="text-lightText font-qregular text-md text-wrap text-center pt-1"
+          numberOfLines={2}
+        >
+          {item.title}
+        </Text>
       </View>
     </TouchableHighlight>
   );
 
   const renderItem = ({ item }) => (
     <TouchableHighlight underlayColor={"transparent"} onPress={() => handlePersonPress(item)}>
-      <View key={item.id} className="w-24 p-2">
+      <View key={item.id} className=" w-28 p-2">
         <Image
           className="h-40 rounded-md"
           source={{ uri: `https://image.tmdb.org/t/p/w500${item.profile_path}` }}
           resizeMode="cover"
         />
-        <Text className="font-qbold text-wrap text-center">{item.name}</Text>
-        <Text className="font-qmedium text-wrap text-center">as {item.character}</Text>
+        <Text
+          className="text-lightText font-qsemibold text-md text-wrap text-center pt-1"
+          numberOfLines={2}
+        >
+          {item.name}
+        </Text>
+        <Text className="text-lightText font-qregular text-md text-wrap text-center">
+          as {item.character}
+        </Text>
       </View>
     </TouchableHighlight>
   );
@@ -156,16 +184,17 @@ const MovieDetail = () => {
       shadowOpacity: 0.27,
       shadowRadius: 4.65,
       elevation: 6,
-      width: "32%",
+      width: "35%",
     },
     contentContainer: {
       flexGrow: 1,
+      paddingBottom: 20,
     },
   });
 
   return (
-    <View className="flex-1">
-      <Modal visible={loading} transparent={true} animationType="fade">
+    <View className="flex-1 bg-bgdark-100">
+      {/*<Modal visible={loading} transparent={true} animationType="fade">
         <View
           style={{
             flex: 1,
@@ -177,25 +206,15 @@ const MovieDetail = () => {
           <ActivityIndicator size="large" color="#ffffff" />
           <Text style={{ color: "#ffffff", marginTop: 10 }}>Loading...</Text>
         </View>
-      </Modal>
+      </Modal>*/}
       {details ? (
         <ScrollView contentContainerStyle={styles.contentContainer}>
-          <View className="flex-1 h-3/5 rounded-bl-lg rounded-br-lg">
+          <View className="flex-1 h-3/5 rounded-bl-xl rounded-br-xl overflow-hidden">
             <ImageBackground
-              style={{ borderBottomLeftRadius: 20 }}
+              imageStyle={{ borderBottomLeftRadius: 50, borderBottomRightRadius: 50 }}
               source={{ uri: `https://image.tmdb.org/t/p/w780${details.backdrop_path}` }}
             >
-              <View className=" bg-black/70 rounded-bl-lg rounded-br-lg pt-10 justify-stretch">
-                {/*<View className="flex-row pl-3 items-center space-x-2">
-                  <TouchableHighlight
-                    className="rounded-2xl w-10 h-10 items-center justify-center"
-                    underlayColor="#ffffff1a"
-                    onPress={() => router.replace("home")}
-                  >
-                    <icons.Left_O height={25} width={25} stroke="#ffffff" />
-                  </TouchableHighlight>
-                  <Text className="text-white font-qmedium text-xl pb-1">Movie Details</Text>
-                </View>*/}
+              <View className=" bg-black/80 rounded-bl-xl rounded-br-xl pt-10 justify-stretch">
                 <View className="flex-1 items-center space-y-4 pt-10">
                   <View className="flex-row justify-between w-full px-5 space-x-5">
                     <Image
@@ -206,20 +225,24 @@ const MovieDetail = () => {
                     />
                     <View className="w-2/3 h-48 justify-between">
                       <Text className="text-white font-qsemibold text-xl">{details.title}</Text>
-                      <View className="flex-row items-center space-x-2">
+                      <View className="flex-row items-center flex-wrap">
+                        {details.genres.slice(0, 3).map((x) => (
+                          <View className="mb-2 mr-2">
+                            <Text
+                              key={x.id}
+                              className="border border-primary bg-primary/20 rounded-lg px-2 py-1 text-white"
+                            >
+                              {x.name}
+                            </Text>
+                          </View>
+                        ))}
+                      </View>
+                      {/*<View className="flex-row items-center space-x-2">
                         <icons.Star_O height={20} width={20} stroke={"#ffffff"} />
                         <Text className="text-white">{details.vote_average.toPrecision(2)}</Text>
-                      </View>
-                      <View className="flex-row items-center space-x-2">
-                        {images.Imdb(12, 20, "#FFEA00")}
-                        <Text className="text-white">{`${imdb}/10`}</Text>
-                      </View>
-                      <View className="flex-row items-center space-x-[12] pl-[1]">
-                        {images.Tomato(16, 16, "#FFC300")}
-                        <Text className="text-white">{tomatometer}</Text>
-                      </View>
+                      </View>*/}
                       <Text className="text-white rounded-md pl-0.5">
-                        {details.adult ? "🔞  A" : "⚠️  U/A"}
+                        {details.adult ? "🔞   A" : "⚠️   U/A"}
                       </Text>
                       <View className="flex-row items-center space-x-2">
                         <icons.Calendar_O height={20} width={20} stroke="#ffffff" />
@@ -230,58 +253,83 @@ const MovieDetail = () => {
                           }/${release.getFullYear()}`}
                         </Text>
                       </View>
+                      <View className="flex-row items-center space-x-2 pr-10">
+                        <icons.Monitor_F height={20} width={20} fill="#ffffff" />
+                        <Text className="text-lightText">
+                          {providers.length > 0 ? "Streaming on " : "-------------"}
+                        </Text>
+                        {providers.length > 0 &&
+                          providers.map((x) => (
+                            <View>
+                              <Image
+                                source={{
+                                  uri: `https://image.tmdb.org/t/p/w500${x.logo_path}`,
+                                }}
+                                className="rounded-md h-5 w-5"
+                              />
+                            </View>
+                          ))}
+                      </View>
                     </View>
                   </View>
-                  <View className="flex-row space-x-2 items-center px-5 w-full">
-                    {details.genres.map((x) => (
-                      <Text
-                        key={x.id}
-                        className="border border-primaryHover bg-primary/20 rounded-lg p-2 text-white"
-                      >
-                        {x.name}
-                      </Text>
-                    ))}
-                  </View>
-                  <Text className="text-white text-justify px-5">{overview}</Text>
+                  <Text className="text-white text-justify px-5 pb-5">{overview}</Text>
                 </View>
               </View>
             </ImageBackground>
           </View>
-          <View className="flex mt-4">
-            <Text>Cast</Text>
+          <View className="flex-row items-center space-x-5 justify-stretch px-5 mt-5">
+            <View className="flex-row items-center space-x-2 border-primary border rounded-xl p-3 grow justify-center bg-primary/5">
+              <images.Imdb_S height={30} width={30} />
+              {loading ? (
+                <ActivityIndicator size="small" color="#ffffff" />
+              ) : (
+                <Text className="text-lightText font-qmedium text-md">
+                  {imdb ? `${imdb}/10` : "--"}
+                </Text>
+              )}
+            </View>
+            <View className="flex-row items-center space-x-3 border-primary border rounded-xl p-3 grow justify-center bg-primary/5">
+              <images.Tomato_S height={30} width={30} />
+              {loading ? (
+                <ActivityIndicator size="small" color="#ffffff" />
+              ) : (
+                <Text className="text-lightText font-qmedium text-">
+                  {tomatometer ? `${tomatometer}%` : "--"}
+                </Text>
+              )}
+            </View>
+          </View>
+          <View>
+            <View className="flex-row items-center py-2 px-4">
+              <icons.Group_F height={20} width={20} fill="#f5cb5c" />
+              <Text className="text-primary font-qmedium text-lg pl-2 pb-1">Cast</Text>
+            </View>
             <FlatList
+              className="px-2"
+              contentContainerStyle={{ paddingRight: 10 }}
               data={credits}
               renderItem={renderItem}
               keyExtractor={(item) => item.id.toString()}
               horizontal
               showsHorizontalScrollIndicator={true}
+              fadingEdgeLength={100}
             />
-            <View style={styles.gradientOverlay}>
-              <LinearGradient
-                colors={["#FFFFFF00", "#FFFFFF"]}
-                start={[0.9, 0]}
-                end={[1, 0]}
-                className="flex-1"
-              />
-            </View>
           </View>
-          <View className="flex-1 mt-4">
-            <Text>Recommendations</Text>
+          <View className="">
+            <View className="flex-row items-center py-2 px-4">
+              <icons.ThumbsUp_F height={20} width={20} fill="#f5cb5c" />
+              <Text className="text-primary font-qmedium text-lg pl-2 pb-1">Recommendations</Text>
+            </View>
             <FlatList
-              data={recoms}
+              class="px-5"
+              contentContainerStyle={{ paddingRight: 10, paddingLeft: 10 }}
+              data={recommendations}
               renderItem={renderRec}
               keyExtractor={(item) => item.id.toString()}
               horizontal
               showsHorizontalScrollIndicator={true}
+              fadingEdgeLength={100}
             />
-            <View style={styles.gradientOverlay}>
-              <LinearGradient
-                colors={["#FFFFFF00", "#FFFFFF"]}
-                start={[0.9, 0]}
-                end={[1, 0]}
-                className="flex-1"
-              />
-            </View>
           </View>
         </ScrollView>
       ) : (
--- app/personDetail.jsx
+++ app/personDetail.jsx
@@ -10,6 +10,7 @@ import {
   StyleSheet,
   TouchableHighlight,
   ActivityIndicator,
+  ImageBackground,
 } from "react-native";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import icons from "../constants/icons";
@@ -18,9 +19,14 @@ import { StatusBar } from "expo-status-bar";
 import { router } from "expo-router";
 
 const PersonDetail = () => {
-  const { deets } = useLocalSearchParams();
+  const { deets, creds } = useLocalSearchParams();
   const [loading, setLoading] = useState(false);
   const details = JSON.parse(deets);
+  const parse = JSON.parse(creds).cast;
+  const credits = parse.sort((a, b) => b.popularity - a.popularity);
+
+  const birthday = new Date(details.birthday);
+  const deathday = new Date(details.deathday);
 
   const styles = StyleSheet.create({
     shadow: {
@@ -32,15 +38,76 @@ const PersonDetail = () => {
       shadowOpacity: 0.27,
       shadowRadius: 4.65,
       elevation: 6,
+      width: "35%",
     },
     contentContainer: {
+      flexGrow: 1,
       paddingBottom: 20,
     },
   });
 
+  const biography = details.biography.split("\n")[0];
+
+  const handleMoviePress = async (item) => {
+    setLoading(true);
+    try {
+      const detailsUrl = `https://api.themoviedb.org/3/movie/${item.id}?language=en-US`;
+      const creditsUrl = `https://api.themoviedb.org/3/movie/${item.id}/credits?language=en-US`;
+      const recsUrl = `https://api.themoviedb.org/3/movie/${item.id}/recommendations?language=en-US`;
+      const responses = await Promise.all([
+        fetch(detailsUrl, options),
+        fetch(creditsUrl, options),
+        fetch(recsUrl, options),
+      ]);
+      const [detailsResponse, creditsResponse, recsResponse] = responses;
+      const detailsJson = await detailsResponse.json();
+      const credJson = await creditsResponse.json();
+      const recsJson = await recsResponse.json();
+      const creditsJson = credJson.cast.sort((a, b) => b.rank - a.rank).slice(0, 10);
+      setLoading(false);
+      router.push({
+        pathname: "movieDetail",
+        params: {
+          deets: JSON.stringify(detailsJson),
+          creds: JSON.stringify(creditsJson),
+          recs: JSON.stringify(recsJson),
+        },
+      });
+    } catch (error) {
+      console.error("Error fetching movie details:", error);
+      setLoading(false);
+    }
+  };
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.EXPO_PUBLIC_TMDB_SECRET}`,
+    },
+  };
+
+  const renderCred = ({ item }) => (
+    <TouchableHighlight underlayColor={"transparent"} onPress={() => handleMoviePress(item)}>
+      <View key={item.id} className="w-28 p-2">
+        <Image
+          className="h-40 rounded-md"
+          source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
+          resizeMode="cover"
+        />
+        <Text
+          className="text-lightText font-qregular text-md text-wrap text-center pt-1"
+          numberOfLines={2}
+        >
+          {item.title}
+        </Text>
+      </View>
+    </TouchableHighlight>
+  );
+
   return (
-    <View className="flex-1">
-      <Modal visible={loading} transparent={true} animationType="fade">
+    <View className="flex-1 bg-bgdark-100">
+      {/*<Modal visible={loading} transparent={true} animationType="fade">
         <View
           style={{
             flex: 1,
@@ -52,34 +119,81 @@ const PersonDetail = () => {
           <ActivityIndicator size="large" color="#ffffff" />
           <Text style={{ color: "#ffffff", marginTop: 10 }}>Loading...</Text>
         </View>
-      </Modal>
+      </Modal>*/}
       {details ? (
         <ScrollView contentContainerStyle={styles.contentContainer}>
-          <View className="max-h-[480]">
-            <Image
-              className="h-full"
+          <View className="flex-1 h-3/5 rounded-bl-xl rounded-br-xl">
+            <ImageBackground
+              imageStyle={{ borderBottomLeftRadius: 50, borderBottomRightRadius: 50 }}
               source={{ uri: `https://image.tmdb.org/t/p/w780${details.profile_path}` }}
-            />
-            <View className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 rounded-bl-lg rounded-br-lg pt-10 justify-stretch">
-              <View className="flex items-center space-y-4 pt-10">
-                <View className="flex-row px-3 gap-3">
-                  <Image
-                    style={styles.shadow}
-                    className="h-40 w-20 rounded-md"
-                    source={{ uri: `https://image.tmdb.org/t/p/w500${details.profile_path}` }}
-                  />
-                  <View className="w-2/3 h-40 justify-between">
-                    <Text className="text-white font-qsemibold text-xl">{details.name}</Text>
+            >
+              <View className=" bg-black/80 rounded-bl-xl rounded-br-xl pt-10 justify-stretch">
+                <View className="flex-1 items-center space-y-4 pt-10">
+                  <View className="flex-row justify-between w-full px-5 space-x-5">
+                    <Image
+                      style={styles.shadow}
+                      className="rounded-md"
+                      resizeMode="stretch"
+                      source={{ uri: `https://image.tmdb.org/t/p/w500${details.profile_path}` }}
+                    />
+                    <View className="w-2/3 h-48 justify-start space-y-5">
+                      <Text className="text-white font-qsemibold text-xl">{details.name}</Text>
+                      <View className="flex-row items-center space-x-2 pr-10">
+                        <icons.Marker_F height={20} width={20} stroke="#ffffff" />
+                        <Text className="text-white">
+                          Born at
+                          {` ${details.place_of_birth}`}
+                        </Text>
+                      </View>
+                      <View className="flex-row items-center space-x-2 pr-10">
+                        <icons.Marker_F height={20} width={20} stroke="#ffffff" />
+                        <Text className="text-white">
+                          Born at
+                          {` ${details.place_of_birth}`}
+                        </Text>
+                      </View>
+                      <View className="flex-row items-center space-x-2">
+                        <icons.Calendar_O height={20} width={20} stroke="#ffffff" />
+                        <Text className="text-white">
+                          Born on
+                          {` ${birthday.getDate()}/${
+                            birthday.getMonth() + 1
+                          }/${birthday.getFullYear()}`}
+                        </Text>
+                      </View>
+                      {details.deathday != null && (
+                        <View className="flex-row items-center space-x-2">
+                          <icons.Calendar_O height={20} width={20} stroke="#ffffff" />
+                          <Text className="text-white">
+                            Born on
+                            {` ${birthday.getDate()}/${
+                              birthday.getMonth() + 1
+                            }/${birthday.getFullYear()}`}
+                          </Text>
+                        </View>
+                      )}
+                    </View>
                   </View>
+                  <Text className="text-white text-justify px-5 pb-5">{biography}</Text>
                 </View>
-                <Text className="text-white">{details.biography}</Text>
-                <Text className="text-white">{details.birthday}</Text>
-                <Text className="text-white">{details.deathday ?? "null"}</Text>
-                <Text className="text-white">{details.gender.toString()}</Text>
-                <Text className="text-white">{details.place_of_birth}</Text>
-                <Text className="text-white">{details.popularity}</Text>
               </View>
+            </ImageBackground>
+          </View>
+          <View className="">
+            <View className="flex-row items-center py-2 px-4">
+              <icons.VideoCamera_F height={20} width={20} fill="#f5cb5c" />
+              <Text className="text-primary font-qmedium text-lg pl-2 pb-1">Appeared in</Text>
             </View>
+            <FlatList
+              class="px-5"
+              contentContainerStyle={{ paddingRight: 10, paddingLeft: 10 }}
+              data={credits}
+              renderItem={renderCred}
+              keyExtractor={(item) => item.id.toString()}
+              horizontal
+              showsHorizontalScrollIndicator={true}
+              fadingEdgeLength={100}
+            />
           </View>
         </ScrollView>
       ) : (
